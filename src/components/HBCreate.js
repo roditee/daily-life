7dde86d417ba4faa5633e9513364c0e13387175c
hope1021 <kdy102130@naver.com>
2022-03-24 01:02:00 +0900

    배너이미지, 리스트아이템

--- src/components/HBCreate.js
+++ src/components/HBCreate.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 import Top from './IndexTop';
 import Bottom from './IndexBottom';
-import './HBCreate.css';
-import './HBCreate2.css';
+// import './HBCreate.css';
+// import './HBCreate2.css';
+import './HBCreate3.css';
 
 const HBCreate = () => {
   let history = useNavigate();
+
+  //로그인한 id 불러오기
+  const state = useSelector((state)=>state)
+
+    const login = state.loginReducer
+    console.log(login)
+
 
   // state
   const [HB, setHB] = useState({ 
@@ -24,6 +33,7 @@ const HBCreate = () => {
       ...HB, // 기존의 HB 객체를 복사한 뒤
       [name]: value // name 키를 가진 값을 value로 설정
     });
+    console.log(HB);
   };
 
   const onReset = () => {
@@ -58,33 +68,39 @@ const HBCreate = () => {
       <section className='section2'>
 
 
-   <div className='cardTitle'>
+  <div className='cardTitle'>
       <h1><span>나</span><span>만</span><span>의</span> <span>행</span><span>복</span><span> </span><span>저</span><span>금</span><span>통</span><span> </span><span>만</span><span>들</span><span>기</span></h1>
-      </div>
+  </div> 
 
       <div class="card">
         
-            <div class="imgBox">
-            <img src='img/wjrmaxhd.gif'/>
-            <div className='haha'>마우스를 올려주세요.</div>
-            </div>
             <div class="details">
                 <form name='frmHBCreateForm' className='frmHBCreateForm' onSubmit={onSubmit} onReset={onReset}>
 
-            <h3>저금통의 이름은 무엇인가요?</h3>
-              <input type='text' name='hbName' className='hbName' value={HB.hbName} onChange={onChange} />
-            <h3>저금통을 개봉할 날짜를 선택해주세요</h3>
-              <input type='date' name='hbEndDate' className='hbEndDate' value={HB.hbEndDate} onChange={onChange} />
-              <h6>* 한 번 설정한 날짜는 변경할 수 없습니다. <br/>
+            <h4>저금통의 이름은 무엇인가요?</h4>
+              <input type='text' name='hbName' className='hbName' value={HB.hbName} onChange={onChange} placeholder='ex. 민초단의 추억' required/>
+
+            <table>
+              <tr>
+                <td>
+                  <h4> 개봉 날짜 : </h4>
+                </td>
+                <td>
+                  <input type='date' name='hbEndDate' className='hbEndDate' value={HB.hbEndDate} onChange={onChange}  required/>
+                </td>
+              </tr>
+            </table>
+
+              <h5>* 한 번 설정한 날짜는 변경할 수 없습니다. <br/>
                   * 개봉날짜 전까지 열어볼 수 없으니 신중하게 선택해주세요.
-              </h6>
+              </h5>
             
             {/* hidden : 회원 이메일 */}
-            <input type='hidden' name='memEmail' onChange={onChange} value={HB.memEmail} />
+            <input type='hidden' name='memEmail' onChange={onChange} value={login} />
             {/* 그외 저금통 번호, 시작날짜는 DB에서 자동으로 들어가도록 했음.*/}
 
             <input className='button1' type='submit' value="등록" />&nbsp;&nbsp;&nbsp;&nbsp;
-            <input className='button1' type='reset' value="취소" />
+            <input className='button2' type='reset' value="취소" />
 
           </form>
                             </div>
